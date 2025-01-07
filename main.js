beca84391107898668bbd4e9176f0d0583196e6c
Shakhrizoda07 <shakhrizoda752@gmail.com>
2025-01-07 09:34:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+//console.log('hello world !!!!!!!!!!!!!!!!!!!          \n salom');
+//var let const
+
+//let myName = 'shakhri'
+
+//var global
+//let local
+//const es6
+
+
+let userName = prompt('введите своё имя')
+let userLastname = prompt('введите свою фамилию')
+let userNumber =prompt( 'введите свой номер')
+let userAge =prompt( 'введите свой возраст')
+let userAddress =prompt( 'введите свой аддресс')
+let userFavoutitesubject =prompt( 'введите свой любимый предмет')
+
+//канкетинация
+//интерполяция
+
+console.log('ваше имя' + userName + '\n ваше фамилия' + userLastname + '\n ваш номер'+ userNumber +'\n ваш возраст' +userAge + '\n ваш аддресс' + userAddress + '\n ваш любимый предмет' +userFavoutitesubject);
+//console.log('ваше имя  ${userName}\n  ваше фамилия ${userLastname}');
+
+
+//console.log(name)
